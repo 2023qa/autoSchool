187ae1fffc2c54908d2e81afc96f8a32597538df
Kristof Angyal <2023qa@gmail.com>
2024-01-23 15:07:04 +0100

    Task: Print HelloWorld

--- src/main/java/practice1/HelloWorld.java
+++ src/main/java/practice1/HelloWorld.java
@@ -1,4 +1,15 @@
 package practice1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        printHelloName();
+    }
+    public static void printHelloName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+    }
 }
