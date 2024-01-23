faa5c9b5a6f0aca43b83b9b0a23420ac0ef5c6d1
Kristof Angyal <2023qa@gmail.com>
2024-01-23 17:04:15 +0100

    Task1: Print HelloWorld! + Hello (user input).

--- src/main/java/practice1/HelloWorld.java
+++ src/main/java/practice1/HelloWorld.java
@@ -3,16 +3,17 @@ package practice1;
 import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         printHelloName();
     }
-    public static void printHelloName(){
-        Scanner userInput = new Scanner(System.in);
+
+    public static void printHelloName() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter your name: ");
-        String name = userInput.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hello " + name + ".");
-        userInput.close();
+        scanner.close();
     }
 }
-                          
+
