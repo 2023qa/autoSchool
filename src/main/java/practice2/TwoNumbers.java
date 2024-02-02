c19ea49a38496413b2222c7912d0ef5ce453526e
Kristof Angyal <2023qa@gmail.com>
2024-02-02 11:29:38 +0100

    Task1: Print HelloWorld! + Hello (user input).

---
+++ src/main/java/practice2/TwoNumbers.java
@@ -0,0 +1,21 @@
+package practice2;
+
+import java.util.Scanner;
+
+public class TwoNumbers {
+    public static void main (String[] args){
+
+        // Two numbers
+        int q = 21;
+        int w = 8;
+
+        // calculation
+
+        int calc1 = q / w;
+        int remainder = q % w;
+
+        //Display result
+        System.out.println(q+"/"+ w + " = " + calc1 + " and the remainder is = " + remainder);
+
+    }
+}
